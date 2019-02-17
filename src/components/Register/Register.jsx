c3b38104c2a9402836c7083249ac771b98db9efc
Vigneshwaran Veeran <vigneshwaran.veeran@appdirect.com>
2019-02-17 01:24:01 -0500

    Login Register and Forgot Password UI added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
+import "../../assets/styles/common.css";
 
 class Register extends Component {
 	render() {
 		return (
-			<h3>Register</h3>
+			<div className="bg-gradient-primary container-with-no-padding">
+				<div className="container">
+					<div className="card o-hidden border-0 shadow-lg my-5">
+						<div className="card-body p-0">
+							<div className="row">
+								<div className="col-lg-5 d-none d-lg-block bg-register-image"></div>
+								<div className="col-lg-7">
+									<div className="p-5">
+										<div className="text-center">
+											<h1 className="h4 text-gray-900 mb-4">Create an Account!</h1>
+										</div>
+										<form className="user">
+											<div className="form-group row">
+												<div className="col-sm-6 mb-3 mb-sm-0">
+													<input type="text" className="form-control form-control-user"
+													       id="exampleFirstName" placeholder="First Name" />
+												</div>
+												<div className="col-sm-6">
+													<input type="text" className="form-control form-control-user"
+													       id="exampleLastName" placeholder="Last Name" />
+												</div>
+											</div>
+											<div className="form-group">
+												<input type="email" className="form-control form-control-user"
+												       id="exampleInputEmail" placeholder="Email Address" />
+											</div>
+											<div className="form-group row">
+												<div className="col-sm-6 mb-3 mb-sm-0">
+													<input type="password" className="form-control form-control-user"
+													       id="exampleInputPassword" placeholder="Password" />
+												</div>
+												<div className="col-sm-6">
+													<input type="password" className="form-control form-control-user"
+													       id="exampleRepeatPassword" placeholder="Repeat Password" />
+												</div>
+											</div>
+											<a href="#" className="btn btn-primary btn-user btn-block">
+												Register Account
+											</a>
+										</form>
+										<hr />
+										<div className="text-center">
+											<a className="small" href="/forgotpassword">Forgot Password?</a>
+										</div>
+										<div className="text-center">
+											<a className="small" href="/login">Already have an account? Login!</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 
+				</div>
+			</div>
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "font-awesome/css/font-awesome.min.css";
+import "./assets/styles/sb-admin-2.css";
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
