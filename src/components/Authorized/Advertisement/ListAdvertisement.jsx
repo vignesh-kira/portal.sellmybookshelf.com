67ae83e38d7c9887976173de6979562ef85f942b
Vigneshwaran Veeran <vigneshwaran.veeran@appdirect.com>
2019-04-07 22:24:44 -0400

    Single List Advertisement UI

--- src/components/Authorized/Advertisement/ListAdvertisement.jsx
+++ src/components/Authorized/Advertisement/ListAdvertisement.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons'
 import SideNav  from '../common/SideNav'
 import TopNav  from '../common/TopNav'
 import PageTitle  from '../common/PageTitle'
@@ -7,6 +9,8 @@ import {withCookies} from "react-cookie";
 import {connect} from "react-redux";
 import {advertisementsListFetch} from "../../../actions/portal";
 import {API_SUCCESS} from "../../../constants/common";
+import "../../../styled/common.css";
+import book from "../../../images/book.png";
 
 class ListAdvertisement extends Component {
 	componentDidMount() {
@@ -14,7 +18,7 @@ class ListAdvertisement extends Component {
 	}
 	render() {
 		const { advertisementsListFetchStatus, advertisementsList } = this.props;
-	
+
 		return (
 			<div id="wrapper">
 				{/* Sidebar */}
@@ -37,9 +41,57 @@ class ListAdvertisement extends Component {
 							{/* Page Heading */}
 							<PageTitle title="Advertisement List"/>
 
-							{/* Content Row */}
-							<div className="row">
+							<div className="col-md-12 text-left"
+							     style={{
+								     border: '1px solid rgba(0, 0, 0, 0.125)',
+								     borderRadius: '5px',
+								     padding: '30px'
+							     }}>
+								{/* Books Count Section */}
+								<div className="row">
+									<h6 className="text-left">1 to 10 of 100 books are listed.</h6>
+								</div>
 
+								{/* Books Advertisement Section */}
+								<div className="row">
+									<div className="card col-md-12 no-padding">
+										<div
+											className="card-body"
+											style={{
+												display: 'flex',
+												flexDirection: 'row',
+												justifyContent: 'space-between'
+											}}>
+											<div style={{display: 'flex'}}>
+												<img
+													src={book} width='100' height='100'
+													style={{marginTop: '-10px'}}
+												/>
+												<div>
+													<h5 className="card-title">&#8377; 320</h5>
+													<p className="card-text">
+														10th Class
+														<br />
+														Science Book
+														<br />
+														<span className="badge badge-pill badge-primary">Available</span>
+													</p>
+												</div>
+											</div>
+											<button
+												style={{alignSelf: 'center'}}
+												className="btn btn-primary"
+											>
+												View
+											</button>
+										</div>
+									</div>
+								</div>
+
+								{/* Pagination Section */}
+								<div className="row">
+
+								</div>
 							</div>
 						</div>
 						{/* /.container-fluid */}
